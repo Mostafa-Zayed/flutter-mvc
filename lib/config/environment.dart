3c6220feb9355c945722c9b560a49f07ec54c63d
Mostafa <mostafa.zayed@gmial.com>
2023-02-15 20:49:48 +0200

    upload project files

---
+++ lib/config/environment.dart
@@ -0,0 +1,22 @@
+/*
+* store app settings
+*/
+Map<String,dynamic> env = {
+  'app_name': 'Erp Tec',
+  'app_title': 'Erp Tec',
+  'app_env': 'demo',
+  'author': 'Mostafa Zayed',
+  'email': 'mostafa.zayed9990@gmail.com',
+  'description': 'this is education system for education and teachers from Erp Tec System Company',
+  'version': '1.0',
+  'module': 'education',
+  'copyright_text': "@ Erp Tec " + DateTime.now().year.toString(),
+  'purchase_ode': '',
+  'default_language': 'en',
+  'mobile_app_code': 'en',
+  'app_language_rtl': false,
+  'https_enabled': true,
+  'domain': 'erptec.net/erp/',
+  'api_prefix': 'api',
+  'api_version': '1',
+};
